16e580ab0eb562d28254bff417c32fa6f4ba4ecf
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2026-05-09 17:15:06 +0900

    a

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -16,7 +16,7 @@ async function getGames(): Promise<Game[]> {
         id: "cube-multiplay",
         title: "큐브 멀티플레이",
         description: "친구들과 함께 즐기는 실시간 멀티플레이 큐브 게임",
-        url: "http://54.116.133.27:3001",
+        url: "http://13.125.187.132:3001",
         emoji: "🎲",
         tags: ["멀티플레이", "실시간"],
         players: null,
