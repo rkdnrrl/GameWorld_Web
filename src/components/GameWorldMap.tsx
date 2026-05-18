4a90da0e37700a5f66d9f8ea36642d087e473ab6
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2026-05-18 18:53:16 +0900

    auto: update GameWorldMap.tsx

--- src/components/GameWorldMap.tsx
+++ src/components/GameWorldMap.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
 import { SESSION_CHANGE_EVENT, session } from "@/lib/api";
 import type { Game } from "./GameCard";
 
