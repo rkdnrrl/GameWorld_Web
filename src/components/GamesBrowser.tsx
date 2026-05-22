81542512d6c40f054bba08d4c7defd071653cdb6
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2026-05-23 06:49:08 +0900

    auto: update GamesBrowser.tsx

--- src/components/GamesBrowser.tsx
+++ src/components/GamesBrowser.tsx
@@ -4,6 +4,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useTranslations } from "next-intl";
 import GameCard, { type Game, type GameCategory } from "@/components/GameCard";
 import AdBanner from "@/components/AdBanner";
+import FeaturedGameBanner from "@/components/FeaturedGameBanner";
 import { SESSION_CHANGE_EVENT, session } from "@/lib/api";
 import { loadLastGameId, saveLastGameId } from "@/lib/lastGame";
 
