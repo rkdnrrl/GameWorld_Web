a9fb66df6734c3428dcb549380ae1ec15a9efa69
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2026-05-23 08:59:56 +0900

    auto: update GameCard.tsx

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
+import { useRouter } from "@/i18n/navigation";
 import { SESSION_CHANGE_EVENT, session } from "@/lib/api";
 import { saveLastGameId } from "@/lib/lastGame";
 
