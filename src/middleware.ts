e427c23ad3b2e08370b652752fec7e77e174c59b
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2026-05-24 20:41:39 +0900

    Delete middleware.ts

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
-
-export default createMiddleware(routing);
-
-export const config = {
-  // api, _next, _vercel, 파일 확장자 제외한 모든 경로에 적용
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
-};
