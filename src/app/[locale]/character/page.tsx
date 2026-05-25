f80565bfae74f00f335407c27cb627ac74a6b63a
rkdnrrl <52886645+rkdnrrl@users.noreply.github.com>
2026-05-25 16:05:14 +0900

    auto: update page.tsx

--- src/app/[locale]/character/page.tsx
+++ src/app/[locale]/character/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useRef, useState, useEffect, useMemo } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 import { useRouter } from '@/i18n/navigation';
 import { useTranslations } from 'next-intl';
 import { session } from '@/lib/api';
